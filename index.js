b2a7cc6935991375bab10ff047361cf7db50bb34
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-11-09 17:42:15 -0300

    feat: creating automated database creation process

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import presentation from './modules/presentation.js'
 import startOptions from './modules/startOptions.js'
+import * as fs from 'node:fs'
+import createDatabase from './services/creatingDb.js'
 
-// Start System
-presentation()
-startOptions()
+const checkBd = fs.existsSync('./pswrec.db')
+if (checkBd == false) {
+    createDatabase()
+} else {
+    // Start System
+    presentation()
+    startOptions()
+}
---
+++ services/creatingDb.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+import sqlite3 from 'sqlite3'
+
+function createDatabase() {
+    let newdb = new sqlite3.Database('pswrec.db', (err) => {
+        if (err) {
+            console.log("Getting error " + err);
+            exit(1);
+        }
+        createDatabase(newdb);
+        newdb.exec("CREATE TABLE PASSWORDS ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,Reference TEXT NOT NULL,Password TEXT NOT NULL)")
+        console.log(chalk.bgGreen("Banco de dados criado com sucesso!\nExecute o aplicativo novamente."))
+        process.exit()
+    });
+}
+
+export default createDatabase
